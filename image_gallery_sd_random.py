f4ce10451a7cd56cdc01c1485099914a8773daee
Deepankar Datta <deepankardatta@nhs.net>
2024-08-11 23:21:29 +0100

    Modified to centre image, more comments

--- image_gallery_sd_random.py
+++ image_gallery_sd_random.py
@@ -10,6 +10,13 @@
 # Mac tips: can open an image in preview, click info, and JFIF tab to see if progressive image
 # This code also ensures that we skip the "." files that MacOS creates on copying files
 
+# Useful references
+# https://github.com/pimoroni/pimoroni-pico/blob/main/micropython/modules_py/inky_frame.md
+# https://github.com/pimoroni/pimoroni-pico/tree/main/micropython/modules/picographics
+
+# Also from Pimoroni: https://learn.pimoroni.com/article/getting-started-with-inky-frame
+# It tells you what size the files need to be
+
 # Make sure to uncomment the correct size for your display!
 from picographics import PicoGraphics, DISPLAY_INKY_FRAME as DISPLAY    # 5.7"
 # from picographics import PicoGraphics, DISPLAY_INKY_FRAME_4 as DISPLAY  # 4.0"
@@ -36,17 +43,56 @@ os.mount(sd, "/sd")
 # Create a new JPEG decoder for our PicoGraphics
 j = jpegdec.JPEG(graphics)
 
+# Variables of the screen pixel size to help centre the pictures later
+# https://learn.pimoroni.com/article/getting-started-with-inky-frame
+# For the 5.7 Inky Frame it is 600(w) x 448(h)
+display_width = 600
+display_height = 448
 
+
+
+
+# Create a function to display the image
 def display_image(filename):
-
+    
     # Open the JPEG file
     j.open_file(filename)
 
     # Decode the JPEG
-    j.decode(0, 0, jpegdec.JPEG_SCALE_FULL)
+    j.decode()
+    # j.decode(0, 0, jpegdec.JPEG_SCALE_FULL) # Original code
+    
+    # Work out the width and height
+    # The Micropython jpegdec module is not amazingly documented
+    # and I am not good at C
+    # However this page: https://github.com/pimoroni/pimoroni-pico/issues/894
+    # hints that there are additional functions that Pimoroni don't use
+    # which are helpful
+    # Note you can only call this after the decoding step
+    picture_width = j.get_width()
+    print(f"Width {picture_width}")
+    picture_height = j.get_height()
+    print(f"Height {j.get_height()}")
+    
+    picture_x_position = round( (display_width - picture_width)/2 )
+    picture_y_position = round( (display_height - picture_height)/2 )
+    
+    # Clear the screen - as if we have image not at full size of screen
+    # it gets displayed with some of the previous image remaining
+    #
+    # note that the picographics manual has this as display.clear()
+    # but we initilaised this as graphics at top
+    graphics.clear()
+    
+    # Decode the JPEG again with instructions where on the screen to
+    # display the picture so that it is centred
+    # Not sure how much RAM this consumes so could garbage collect
+    j.decode( picture_x_position , picture_y_position , jpegdec.JPEG_SCALE_FULL )
 
     # Display the result
     graphics.update()
+
+
 
 
 inky_frame.led_busy.on()
@@ -60,6 +106,9 @@ files = [f for f in files if not f.startswith(".")]
 # initial variable "lastfile" so that we don't select the same picture twice in a row
 lastfile = ""
 
+
+
+# Main lop for program
 while True:
     
     # pick a random file
@@ -75,11 +124,6 @@ while True:
 
     # Open the file
     print(f"Displaying /sd/{file}")
-    
-    # Clear the screen
-    # note that the picographics manual has this as display.clear()
-    # but we initilaised this as graphics at top
-    graphics.clear()
     
     # Display the image
     display_image("/sd/" + file) 
